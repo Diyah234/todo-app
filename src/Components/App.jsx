8308b4328f6fae954f4a9ce39da0925b55ff8b3f
Abudu Rodiyyah <rodiyahabudu@gmail.com>
2024-03-17 20:03:48 +0100

    finished

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,36 +1,47 @@
 import React, { useState } from 'react';
-import './style.css';
+import '../style.css';
 import dark from '../images/bg-desktop-dark.jpg';
 import sun from '../images/icon-sun.svg';
 import cross from '../images/icon-cross.svg';
- 
+import mobiledark from '../images/bg-mobile-dark.jpg';
+import light from '../images/bg-desktop-light.jpg';
+import mobilelight from '../images/bg-mobile-light.jpg';
+
 function App() {
   // State hooks to manage input text, todo items, and filter
   const [inputText, setInputText] = useState('');
   const [items, setItems] = useState([]);
   const [filter, setFilter] = useState('All');
- 
+
+  const [lightMode, setLightMode] = useState(false);
+
+  const handleLightModeChange = () => {
+    setLightMode(!lightMode);
+  };
   // Function to handle filter selection
   function handleFilter(filter) {
     setFilter(filter);
   }
- 
+
   // Function to handle input change
   function handleChange(event) {
     setInputText(event.target.value);
   }
- 
+
   // Function to add a new todo item
   function addItem() {
     // Check if input is not empty
     if (inputText.trim() !== '') {
       // Add the new todo item to the list
-      setItems(prevItems => [...prevItems, { text: inputText, completed: false }]);
+      setItems((prevItems) => [
+        ...prevItems,
+        { text: inputText, completed: false },
+      ]);
       // Clear the input field
       setInputText('');
     }
   }
- 
+
   // Function to toggle completion status of a todo item
   function toggleComplete(index) {
     // Create a copy of the items array
@@ -40,7 +51,7 @@ function App() {
     // Update the state with the modified items array
     setItems(updatedItems);
   }
- 
+
   // Function to delete a todo item
   function deleteItem(index) {
     // Filter out the item to be deleted
@@ -48,17 +59,17 @@ function App() {
     // Update the state with the modified items array
     setItems(updatedItems);
   }
- 
-   // Function to clear completed todo items
-   function clearCompleted() {
+
+  // Function to clear completed todo items
+  function clearCompleted() {
     // Filter out the completed items
-    const remainingItems = items.filter(item => !item.completed);
+    const remainingItems = items.filter((item) => !item.completed);
     // Update the state with the remaining items
     setItems(remainingItems);
   }
 
   // Filter the items based on the selected filter
-  const filteredItems = items.filter(todoItem => {
+  const filteredItems = items.filter((todoItem) => {
     if (filter === 'All') {
       return true;
     } else if (filter === 'Active') {
@@ -68,24 +79,81 @@ function App() {
     }
     return false;
   });
+
+  const mobile = window.matchMedia('(max-width: 475px)');
+
+  // const darkModeMobile = mobile ? url(${mobiledark}) : url(${dark});
+
+  // const lightModelMobile = mobile ? url(${mobilelight}) : url(${light});
+
+  let darkModeMobile;
+  let lightModelMobile;
+
+  if (mobile === true) {
+    darkModeMobile = `url(${mobiledark})`;
+    lightModelMobile = `url(${mobilelight})`;
+  } else {
+    darkModeMobile = `url(${dark})`;
+    lightModelMobile = `url(${light})`;
+  }
  
   return (
-    <div className="App">
-      <div className='bg'><img src={dark} alt="dark background" /></div>
-      <div className='black'></div>
-      <div className='box'>
-        <div className='flex todo'><h1>TODO</h1><img src={sun} alt="sun icon" /></div>
-        <div className='flex inp'>
-          <div><input type="text" placeholder='Create a new todo...' value={inputText} onChange={handleChange} /></div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        width: '100%',
+        overflowX: 'hidden',
+      }}
+    >
+      <div
+        style={{
+          backgroundImage: lightMode ? lightModelMobile : darkModeMobile,
+          backgroundRepeat: 'no-repeat',
+          flex: 1,
+        }}className='bg'
+      ></div>
+      <div
+        style={{
+          backgroundColor: lightMode ? '#f0f4f6' : '#25273c',
+          flex: 1,
+        }} className='dark'
+      ></div>{' '}
+      <div className="box">
+        <div className="flex todo">
+          <h1>TODO</h1>
+          <img src={sun} alt="sun icon" onClick={handleLightModeChange} />
+        </div>
+        <div className="flex inp" style={{backgroundColor: lightMode ? '#f0f4f6' : '#25273c'}}>
+          <div>
+            <input
+              type="text"
+              placeholder="Create a new todo..."
+              value={inputText}
+              onChange={handleChange}
+              style={{backgroundColor: lightMode ? '#f0f4f6' : '#25273c', color: lightMode ? 'hsl(235, 19%, 35%)': 'hsl(0, 0%, 98%)'}}
+            />
+          </div>
           <button onClick={addItem}>ADD</button>
         </div>
-        <div className='ctn'>
+        <div className="ctn" style={{backgroundColor: lightMode ? '#f0f4f6' : '#25273c'}}>
           {filteredItems.map((todoItem, index) => (
-            <div className='flex ctn1' key={index}>
-              <div className='flex2'>
+            <div className="flex ctn1" key={index}>
+              <div className="flex2">
                 {/* Clicking on the circle toggles completion status */}
-                <div className={todoItem.completed ? 'selected' : 'circle'} onClick={() => toggleComplete(index)}></div>
-                <span style={{ textDecoration: todoItem.completed ? 'line-through' : 'none' }}>
+                <div
+                  className={todoItem.completed ? 'selected' : 'circle'}
+                  onClick={() => toggleComplete(index)}
+                ></div>
+                <span
+                  style={{
+                    textDecoration: todoItem.completed
+                      ? 'line-through'
+                      : 'none',
+                      color: lightMode ? 'hsl(235, 19%, 35%)': 'hsl(0, 0%, 98%)'
+                  }}
+                >
                   {todoItem.text}
                 </span>
               </div>
@@ -93,16 +161,23 @@ function App() {
               <img src={cross} onClick={() => deleteItem(index)} alt="delete" />
             </div>
           ))}
-          <div className='flex last'>
+          <div className="flex last">
             {/* Display the number of active items */}
-            <div><p><span>{items.filter(item => !item.completed).length}</span> items left</p></div>
-            <div className='flex2'>
-              {/* Filter buttons */}
-              <p onClick={() => handleFilter('All')}>All</p>
-              <p onClick={() => handleFilter('Active')}>Active</p>
-              <p onClick={() => handleFilter('Completed')}>Completed</p>
+            <div>
+              <p>
+                <span>{items.filter((item) => !item.completed).length}</span>{' '}
+                items left
+              </p>
             </div>
-            <div><p onClick={clearCompleted}>Clear Completed</p></div>
+            <div className="flex2">
+              {/* Filter buttons */}
+              <p onClick={() => handleFilter('All')} style={{color: filter ==='All' ? 'hsl(220, 98%, 61%)' : 'inherit'}} className={filter !== 'All' ? 'hovers' : ''}>All</p>
+              <p onClick={() => handleFilter('Active')} style={{color: filter ==='Active' ? 'hsl(220, 98%, 61%)' : 'inherit'}} className={filter !== 'Active' ? 'hovers' : ''}>Active</p>
+              <p onClick={() => handleFilter('Completed')} style={{color: filter ==='Completed' ? 'hsl(220, 98%, 61%)' : 'inherit'}} className={filter !== 'Completed' ? 'hovers' : ''}>Completed</p>
+            </div>
+            <div>
+              <p onClick={clearCompleted} className={lightMode ? 'no-hover' : 'hovers'}>Clear Completed</p>
+            </div>
           </div>
         </div>
       </div>
